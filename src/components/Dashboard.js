24b207cb0645b2f273e342b289b54a72d4dfdccf
Peter Olumolu <olumolupeter@gmail.com>
2021-05-03 10:44:31 +0100

    feat: Display answered and unanswered questions
    The Dashboard now displays both answered and unanswered questions using a tab.
    built a helper to handle checking if a user has answered a question.
    bugfix.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import Question from './Question';
+import {hasAnswered} from '../utils/helpers'
 
-function Dashboard(props) {
-    const {unAnswered, answered} = props;
+class Dashboard extends Component {
+    state = {
+        clicked: 'unanswered',
+    }
 
-    return (
-        <div className=''>
-            {unAnswered.map((id) => (
-                <Question key={id} id={id} />
-            ))}
-        </div>
-    )
+    handleClick = (e) => {
+        e.preventDefault();
+        const clicked = e.target.name;
+
+        this.setState({
+            clicked
+        });
+    }
+
+    render () {
+        const {unAnswered, answered} = this.props;
+        const {clicked} = this.state;
+        const questions = clicked === 'unanswered' ? unAnswered : answered;
+
+        return (
+            <div className='box'>
+                <div className='tab center'>
+                    <button className={`tablinks ${clicked === 'unanswered' ? 'active': ''}`} name='unanswered' onClick={this.handleClick}>Unanswered Questions</button>
+                    <button className={`tablinks ${clicked === 'answered' ? 'active': ''}`} name='answered' onClick={this.handleClick}>Answered Questions</button>
+                </div>
+                <div className='tabcontent'>
+                    {questions.map((id) => (
+                        <Question key={id} id={id} />
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
 
 function mapStateToProps({questions, authedUser}) {
 
     const unAnswered = Object.keys(questions)
+                        .filter((key) => !hasAnswered(questions[key], authedUser))
                         .sort((a,b) => questions[b].timestamp - questions[a].timestamp);
     const answered = Object.keys(questions)
+                        .filter((key) => hasAnswered(questions[key], authedUser))
                         .sort((a,b) => questions[b].timestamp - questions[a].timestamp);
 
     return {
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,9 +5,15 @@ export function formatDate (timestamp) {
   return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
 }
 
+//Return a user's leaderboard score.
 export function score(user) {
   const questions = user.questions.length;
   const answers = Object.keys(user.answers).length;
 
   return questions + answers;
+}
+
+//Returns true if a user has answered a question and false otherwise
+export function hasAnswered(question, authedUser) {
+  return question.optionOne.votes.includes(authedUser) || question.optionTwo.votes.includes(authedUser);
 }
